243cf6bd612e9e04c7781ed2f8c68695be9579b1
thetsunamisam <thetsunamisam@gmail.com>
2021-11-18 18:38:00 -0600

    modified:   gui.py 	modified:   openflix.py

--- openflix.py
+++ openflix.py
@@ -42,11 +42,11 @@ class Command():
         client.load_system_host_keys()
         client.connect(hostname='cellcraft.us.to',port=55892,username="zorg")
         print("Connected to " + ip)
-        stdin, stdout, standerr = client.exec_command("ls -l -h /media/zorg/Big_Chungus/Movie_Database")
+        stdin, stdout, standerr = client.exec_command("ls -l -h /home/zorg/Movie_Database/Movie_Database/")
         lineout = stdout.readlines()
         for items in lineout:
             print (items)
-
+        return lineout
 #send = Up()
 #send.upload(clientDir = "openflix.py", remoteDir = "/home/zorg")
 #fgfjfgkfgkjfgkjgrecv = Down()
